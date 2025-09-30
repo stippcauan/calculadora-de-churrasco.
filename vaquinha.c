b5e23dc44749ddc08e64b72ad8508d36e3c9286f
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2025-09-30 12:02:41 +0000

    .

--- vaquinha.c
+++ vaquinha.c
@@ -5,7 +5,7 @@ float totalArrecadado = 0;
 // Procedimento:adiciona a contribuição de um participante
 void adicionaContribuicao(float valor){
     totalArrecadado += valor;
-    printf("Contribuição de R$ %.2f adiciona! Total: R$ %.2f, valor, totalArrecadado");
+    printf("Contribuição de R$ %.2f adiciona! Total: R$ %.2f", valor, totalArrecadado);
 }
 
 //função: calcula quantidade de carne por pessoas(0.5kg por pessoa)
@@ -13,8 +13,6 @@ void adicionaContribuicao(float valor){
 float calculaCarne(int qtdPessoas){
     return qtdPessoas * 0.5;
 }
-
-
 
 
 // Procedimento para mostrar o resumo
@@ -26,9 +24,9 @@ void mostraResumo(int qtdPessoas){
     printf("Carne necessária : %2f KG\n", carne);
     printf("Total arrecadado: R$ %.2f\n", totalArrecadado);
     printf("Média por pessoa: R$ %.2f\n", média);
-    printf("================================")
-}
+    printf("================================\n");
 
+}
 
 int main(){
  int pessoas;
@@ -36,6 +34,12 @@ int main(){
 
  printf("Digite o número de participantes: ");
  scanf("%d",&pessoas);
+
+ for (int i =1 ; i <= pessoas; i++){
+    printf("Digite a contribuição do participante %d: R4$ ", i);
+    scanf("%f", &valor);
+    adicionaContribuicao(valor);
+ }
 
  mostraResumo(pessoas);
 }
